147f7e234fd16d86b446a8519c4e6e08a6848460
James McKinney <james@slashpoundbang.com>
2012-09-27 14:56:09 -0400

    rename "divisions" route to "places"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Popolo::Engine.routes.draw do
-  resources :divisions, only: [:index, :show]
+  resources :places, only: [:index, :show], controller: 'divisions'
   resources :parties, only: [:index, :show]
   resources :people, only: [:index, :show]
 end
--- spec/routing/popolo/divisions_routing_spec.rb
+++ spec/routing/popolo/divisions_routing_spec.rb
@@ -7,15 +7,15 @@ describe Popolo::DivisionsController do
     end
 
     it 'routes to #index' do
-      get('/divisions').should route_to('popolo/divisions#index')
+      get('/places').should route_to('popolo/divisions#index')
     end
 
     it 'routes to #show' do
-      get('/divisions/1').should route_to('popolo/divisions#show', id: '1')
+      get('/places/1').should route_to('popolo/divisions#show', id: '1')
     end
 
     it 'routes to #show' do
-      get('/divisions/foo').should route_to('popolo/divisions#show', id: 'foo')
+      get('/places/foo').should route_to('popolo/divisions#show', id: 'foo')
     end
   end
 end
