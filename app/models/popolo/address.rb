d683596fb589ab30bf3ce93a5d3cdf3a25562d65
James McKinney <james@slashpoundbang.com>
2012-10-14 16:29:26 -0400

    properly place Address comment

--- app/models/popolo/address.rb
+++ app/models/popolo/address.rb
@@ -1,28 +1,30 @@
-# Turtle document:
-#
-#     <http://example.com/people/47cc67093475061e3d95369d.ttl>
-#       v:hasAddress [
-#         a v:Address;
-#         v:label "10-123 1/2 Main St\n1/2 Main St SE\nMontreal QC  H3Z 2Y7";
-#         v:context [ rdfs:label "Constituency address" ]
-#       ];
-#       v:hasTelephone [
-#         a v:Telephone;
-#         v:telephone "tel:+1-800-555-0100;ext=555";
-#         v:phone [ a v:Voice ]
-#       ];
-#       v:hasTelephone [
-#         a v:Telephone;
-#         v:telephone "tel:+1-800-555-0199";
-#         v:phone [ a v:Fax ]
-#       ].
-#
-# @see http://www.w3.org/wiki/RepresentingVCardinRDFOWL
-# @see http://tools.ietf.org/html/rfc6350
-# @see http://tools.ietf.org/html/rfc3966
-# @see http://www.iana.org/assignments/vcard-elements/vcard-elements.xml
 module Popolo
-  # An office including postal address and telephone and facsimile numbers.
+  # A contact point including postal address and telephone and fax numbers.
+  #
+  # Turtle document:
+  #
+  #     <http://example.com/people/47cc67093475061e3d95369d.ttl>
+  #       v:hasAddress [
+  #         a v:Address;
+  #         v:label "10-123 1/2 Main St\n1/2 Main St SE\nMontreal QC  H3Z 2Y7";
+  #         v:context [ rdfs:label "Constituency address" ]
+  #       ];
+  #       v:hasTelephone [
+  #         a v:Telephone;
+  #         v:telephone "tel:+1-800-555-0100;ext=555";
+  #         v:phone [ a v:Voice ]
+  #       ];
+  #       v:hasTelephone [
+  #         a v:Telephone;
+  #         v:telephone "tel:+1-800-555-0199";
+  #         v:phone [ a v:Fax ]
+  #       ].
+  #
+  # @see http://www.w3.org/wiki/RepresentingVCardinRDFOWL
+  # @see http://tools.ietf.org/html/rfc6350
+  # @see http://tools.ietf.org/html/rfc3966
+  # @see http://www.iana.org/assignments/vcard-elements/vcard-elements.xml
+  # @see http://schema.org/ContactPoint
   class Address
     include Mongoid::Document
 
