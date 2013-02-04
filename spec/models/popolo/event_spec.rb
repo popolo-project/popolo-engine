8c77d8d2ca2b49d68fc9e8421e0e94bb4aff619e
James McKinney <james@slashpoundbang.com>
2013-02-04 14:23:53 -0500

    remove sources and events for now

--- spec/models/popolo/event_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Popolo::Event do
-  [:source, :url, :body, :issued_at].each do |attribute|
-    it {should validate_presence_of attribute}
-  end
-end
--- spec/models/popolo/source_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Popolo::Source do
-  [:name, :last_modified].each do |attribute|
-    it {should validate_presence_of attribute}
-  end
-end
