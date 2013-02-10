1d37b852bc278a24dab725825e0e069be16a521e
James McKinney <james@slashpoundbang.com>
2013-02-10 17:27:13 -0500

    render 404 without layout

--- lib/generators/popolo_generator.rb
+++ lib/generators/popolo_generator.rb
@@ -18,7 +18,7 @@ module Popolo
           <<-EOS
   rescue_from Mongoid::Errors::DocumentNotFound do |exception|
     respond_to do |format|
-      format.html { render file: Rails.root.join('public', '404.html'), status: :not_found }
+      format.html { render file: Rails.root.join('public', '404.html'), status: :not_found, layout: false }
       format.json { head :not_found }
       format.atom { head :not_found }
     end
