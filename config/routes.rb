2f01db45994ac63f7e7733266ec4902df22adef7
James McKinney <james@slashpoundbang.com>
2014-02-17 16:46:34 -0500

    allow slashes in :id in paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Popolo::Engine.routes.draw do
-  resources :organizations, only: [:index, :show] do
+  resources :organizations, only: [:index, :show], id: /.*/ do
     resources :posts, only: [:index, :show]
   end
-  resources :people, only: [:index, :show]
-  resources :posts, only: [:index, :show]
+  resources :people, only: [:index, :show], id: /.*/
+  resources :posts, only: [:index, :show], id: /.*/
 
   # A host application will sometimes complain about a missing `root_path`, even
   # if it defines a `root_path`. Setting a reasonable default here.
