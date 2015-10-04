e44a50cbd4edc4fa8e631dd2a45735753a916d32
James McKinney <james@slashpoundbang.com>
2015-10-04 15:08:25 -0400

    update coveralls config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'spec'
+end
 
 require 'spec_helper'
 require File.expand_path("../dummy/config/environment", __FILE__)
